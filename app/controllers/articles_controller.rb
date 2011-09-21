7f01380c385b183f36de9ae0e5dd345ca17835a5
Steven Klise <steven@stevenklise.com>
2011-09-21 01:09:01 -0400

    Markdown on article body

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
   
   def index
-    @articles = Article.all
+    @articles = Article.find(:all, :order => 'created_at DESC')
   end
 
   def show
@@ -16,6 +16,18 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
-  
-  
+  def create
+    @article = Article.new(params[:article])
+    
+    respond_to do |format|
+      if @article.save
+        format.html { redirect_to @article, notice: 'article was successfully created.' }
+        format.json { render json: @article, status: :created, location: @article }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 end
